aa890b2f94c0554a1a711a540f54bd459984724c
Felix <felixgerick@gmail.com>
2022-04-10 19:56:07 +0200

    test comparison simplify

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ using Test
 @testset "SymbolicRubi.jl" begin
     @syms x m
     @test isequal(integrate(x^-1,x) |> r1, log(x))
-    @test isequal(integrate(x^m,x) |> r2, x^(m+1)*(m+1)^-1)
+    @test isequal(integrate(x^m,x) |> r2 |> simplify, x^(m+1)*(m+1)^-1 |> simplify)
 
 end
