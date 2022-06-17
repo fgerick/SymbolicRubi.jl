2041e113e42be081264be74959ed804b69c14e02
Florian Atteneder <florian.atteneder@gmail.com>
2022-06-17 13:04:26 +0200

    report required functions for each file

--- gen/scripts/gather_functions.jl
+++ gen/scripts/gather_functions.jl
@@ -1,6 +1,6 @@
 using SymbolicRubi
 
-fns = String[]
+fnslist = Dict{String,Vector{String}}()
 fncallrgx = r"(\w+)\["
 startcodergx = r"^\(* Code *\)$"
 
@@ -10,34 +10,37 @@ rubitestdir = normpath(joinpath(thisdir, "..", "RubiTestFiles"))
 
 for rubidir in [ rubirulesdir, rubitestdir ]
   for (root, dirs, files) in walkdir(rubidir)
+    fns = String[]
     for file in files
       !endswith(file, ".m") && continue
-      fname = joinpath(root,file)
-      f = open(fname, "r")
-      iter = readlines(f)
-      for line in iter
-        if match(startcodergx, line) !== nothing
-          break
+      fname = joinpath(root, file)
+      open(fname, "r") do f
+        iter = readlines(f)
+        for line in iter
+          # skip any headers
+          startswith(line, startcodergx) && break
         end
-      end
-
-      for line in iter
-        for m in eachmatch(fncallrgx, line)
-          fn = first(m.captures)
-          if !in(fn, fns)
-            push!(fns, first(m.captures))
+        for line in iter
+          for m in eachmatch(fncallrgx, line)
+            fn = first(m.captures)
+            if !in(fn, fns)
+              push!(fns, first(m.captures))
+            end
           end
         end
       end
-
-      close(f)
+      fnslist[fname] = deepcopy(fns)
     end
   end
 end
 
+
+# figure out which functions are already defined
+allfns = vcat(values(fnslist)...)
+unique!(allfns)
 defined_fns = String[]
 missing_fns = String[]
-for fn in fns
+for fn in allfns
   if isdefined(SymbolicRubi, Symbol(fn))
     push!(defined_fns, fn)
   else
@@ -45,23 +48,43 @@ for fn in fns
   end
 end
 
+# figure out which files require the least functions to be working
+vec_fnslist = [ (k,v) for (k,v) in pairs(fnslist) ]
+sort!(vec_fnslist, by=kv->length(kv[2]))
 
-required_functions = joinpath(thisdir, "required_functions.jl")
-f = open(required_functions, "w")
-print(f, """
-=========================================
-Defined functions
-=========================================
-""")
-for fn in defined_fns
-  println(f, fn)
+fname_required_functions = joinpath(thisdir, "required_functions.md")
+open(fname_required_functions, "w") do io
+  print(io, """
+  =========================================
+  Defined functions
+  =========================================
+  """)
+  for fn in defined_fns
+    println(io, fn)
+  end
+  println(io)
+  print(io, """
+  =========================================
+  Missing functions
+  =========================================
+  """)
+  for fn in missing_fns
+    println(io, fn)
+  end
+  println(io)
+  print(io, """
+  =========================================
+  Functions required for each file
+  =========================================
+  """)
+  println(io)
+  for (fname, fns) in vec_fnslist
+    print(io, """
+    File: $fname
+    """)
+    for fn in fns
+      println(io, fn)
+    end
+    println(io)
+  end
 end
-print(f, """
-=========================================
-Missing functions
-=========================================
-""")
-for fn in missing_fns
-  println(f, fn)
-end
-close(f)
